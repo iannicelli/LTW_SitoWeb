c6e7877aff413a665382e817130260fd286cdcdb
Chiara Iannicelli <chiara.iannicelli30@gmail.com>
2023-05-03 18:51:36 +0200

    3magg pomeriggio

--- SQL/creazione tabella.sql
+++ SQL/creazione tabella.sql
@@ -4,3 +4,15 @@ CREATE TABLE utenti (
     email VARCHAR(100) NOT NULL primary key,
     password VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE utenti
+ADD COLUMN n_adottati INT CHECK (n_adottati >= 0) ,
+ADD COLUMN n_caricati INT CHECK (n_caricati >= 0) ;
+
+ALTER TABLE utenti
+ALTER COLUMN n_adottati SET DEFAULT 0,
+ALTER COLUMN n_caricati SET DEFAULT 0;
+
+select * from utenti;
+
+
--- prova.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo 'assa fa';
-?>
